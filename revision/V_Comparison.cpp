d08547bba31d3142e52b97db5c5af52394d3d11e
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-06-11 23:53:55 +0600

    comparison problem solution added

---
+++ revision/V_Comparison.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    char s;
+    cin >> a >> s >> b;
+    if (s == '>')
+    {
+        (a > b) ? cout << "Right"
+                : cout << "Wrong";
+    }
+    if (s == '<')
+    {
+        (a < b) ? cout << "Right"
+                : cout << "Wrong";
+    }
+    if (s == '=')
+    {
+        (a == b) ? cout << "Right"
+                : cout << "Wrong";
+    }
+    return 0;
+}
